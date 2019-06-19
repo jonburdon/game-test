8028d6f88816cacd9586f245e808a42d40cd550d
jonburdon <jb@burdonworld.net>
2019-06-19 10:58:10 +0100

    create nim2 test version

---
+++ nim2.js
@@ -0,0 +1,68 @@
+
+
+NimGame = function() {
+
+    var nimstaken = 0; // Keep track of how many nims have been taken in this turn
+    var p1turn = true; // Keep track of whose turn it is
+    var nimsinplay = 21; // Keep track of overall number of nims
+
+increaseNimsTaken = function() {
+//Display number of nims
+document.getElementById("nimnumber").innerHTML = nimsinplay;
+
+    // Hide Button
+
+    // Increase number of counters taken
+
+    nimstaken = nimstaken+1;
+    document.getElementById("nimstaken").innerHTML = nimstaken;
+}
+
+reduceOverallNims = function() {
+    // Reduce overall Nims
+
+    nimsinplay = nimsinplay-1;
+    document.getElementById("nimnumber").innerHTML = nimsinplay;
+}
+
+passPlayOrNot = function {
+// Pass play to other player after three counters taken
+
+if (nimstaken === 3) {
+    if (p1turn === true) {
+        p1turn = false;
+        document.getElementById("whoseturn").innerHTML = "Player 2s Turn";
+        nimstaken = 0;
+    }
+    else {
+        p1turn = true;
+        document.getElementById("whoseturn").innerHTML = "Player 1s Turn";
+        nimstaken = 0;
+    }
+    
+}
+}
+
+checkForWin = function {
+    // Check for win
+
+    if (nimsinplay === 0) {
+        // Declare the winner
+        if (p1turn === true) {
+           document.getElementById("nimwinner").innerHTML = "Player One Wins";
+        }
+        else {
+           document.getElementById("nimwinner").innerHTML = "Player Two Wins";
+        }
+        
+   }
+}    
+
+
+};
+
+PassTurn = function() {
+    // Check if at least one nim has been taken
+
+    // Allow play to be passed to other player
+}
