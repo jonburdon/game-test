a7e23c037074c70a5085fccdec42c533446a7248
jonburdon <jb@burdonworld.net>
2019-06-19 16:40:19 +0100

    keep function not global

--- assets/jasmine-testing/spec/nimspec.js
+++ assets/jasmine-testing/spec/nimspec.js
@@ -1,15 +1,15 @@
 describe("Nim Game", function() {
 
     describe("Checks Easy Nim game has code", function() {
-        it("should exist", function() {
-            expect(increaseCountersTaken).toBeDefined();
+        fit("should exist", function() {
+            expect(nimGame.increaseCountersTaken).toBeDefined();
         });
     });
     
     describe("Checks Easy Nim Game increases Nims taken by 1", function() {
         it("should increase nimstaken from 0 to 1", function() {
             
-            var result increaseCountersTaken(0)
+            var result = nimGame.increaseCountersTaken(0)
 
             expect(result).toBe(1);
         });
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,23 @@
 
 
-increaseCountersTaken = function (counts) {
-    counts++;
-    return counts;
-};
+
+
+var nimGame = (function () {
+
+
+
+    return { 
+    
+    
+        var nimstaken = 0;
+
+        increaseCountersTaken = function (counts) {
+            counts++;
+            return counts;
+        };
+
+
+    }
+
+}());
+
